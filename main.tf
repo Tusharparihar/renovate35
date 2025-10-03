ed32a55c8ea21cdec60fb08d49ecaad99ebce18e
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-10-03 12:10:10 +0530

    Update and rename renovate.json to main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.0"
+    }
+  }
+}
