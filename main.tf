088115c02f51a5690beaa975ab1f4296d33e16d4
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-10-03 13:23:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "~> 2.80"
     }
     random = {
       source  = "hashicorp/random"
